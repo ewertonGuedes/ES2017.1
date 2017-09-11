8e7f010602419127b69ba73b9ac8924bd6e47dee
luandersan.maria <luandersan.maria@LAB2-DCX-23.dcx.ufpb.br>
2017-09-11 11:32:46 -0300

    Luandersan

--- src/Ewerton.java
+++ src/Ewerton.java
@@ -4,6 +4,7 @@ public class Ewerton {
 	public static void main(String[] args) {
 		System.out.println("Engineering Class");
 		System.out.println("Thiago Class");
+		System.out.println("Luandersan Class");
 
 	}
 
